7bd4975749126cfa5eb3352058e0639f5e098825
Andreas Karlsson <andreas.karlsson3@gmail.com>
2016-01-18 20:50:34 +0100

    Add libraries

--- src/Program.fs
+++ src/Program.fs
@@ -1,6 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open FSpotify
+open RedditSharp
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
