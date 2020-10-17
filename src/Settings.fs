220ca025c7781bbe0d0dd2c684aab6d99b352742
Andreas Karlsson <andreas@jhkarlsson.com>
2020-10-17 18:31:55 +0200

    Add .net core console application

--- src/Settings.fs
+++ src/Settings.fs
@@ -3,6 +3,26 @@
 open FSpotify
 open FSharp.Data
 
-type Settings = XmlProvider<"settings.xml">
+[<Literal>]
+let sample = """<?xml version="1.0" encoding="utf-8" ?>
+<setting>
+  <authorization>
+    <client id="" secret="" />
+    <token
+        value=""
+        refresh=""
+        type="Bearer"
+        expires="3600" />
+  </authorization>
+  <jobs>
+    <job id="listentothis-hot" refresh="600">
+      <playlist user="" id="" limit="100"/>
+      <subreddit name="listentothis" pattern="(?&lt;artist&gt;.*?)-+(?&lt;title&gt;.*?)\[" limit="25"/>
+    </job>
+  </jobs>
+</setting>
+"""
+
+type Settings = XmlProvider<sample>
 
 let settings = Settings.Load "settings.xml"
---
+++ src/console/Program.fs
@@ -0,0 +1,16 @@
+﻿
+
+open RedditSharp
+open Settings
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Bot started"
+
+    let reddit = new Reddit()
+
+    while true do
+        try Job.run reddit settings.Job |> Async.RunSynchronously
+        with e ->
+            printfn "There was an error in the bot, restarting..."
+    0
